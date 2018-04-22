8a56261aeffe7e3a988440155c1e8ce44a668b96
José M. Pérez <jperez@spotify.com>
2018-04-22 16:49:50 +0200

    Add Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -30,6 +30,12 @@ export default class IntlDocument extends Document {
             }}
           />
           <NextScript />
+          <script
+            dangerouslySetInnerHTML={{
+              __html:
+                "(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;e=o.createElement(i);r=o.getElementsByTagName(i)[0];e.src='//www.google-analytics.com/analytics.js';r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));ga('create','UA-39254352-1');ga('send','pageview');"
+            }}
+          />
         </body>
       </html>
     );
