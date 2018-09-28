62cf168fa4b843980e98a88ca61235df73839032
José M. Pérez <jperez@spotify.com>
2018-09-28 07:32:01 +0200

    Language and deps updates

--- pages/about.js
+++ pages/about.js
@@ -9,17 +9,26 @@ const About = () => (
     <style jsx>
       {`
         p {
-          margin: 20px;
-          padding: 20px;
+          margin: 20px 40px 0;
         }
       `}
     </style>
+
     <p>
       This is a personal project by <a href="https://jmperezperez.com">José M. Pérez</a> that uses{' '}
       <a href="https://github.com/zeit/next.js">Next.js</a> and{' '}
       <a href="https://developer.spotify.com/web-api/web-api-connect-endpoint-reference/">
-        Spotify's Connect API endpoints.
-      </a>. You can see the source code <a href="https://github.com/JMPerez/c">on GitHub</a>.
+        Spotify's Connect API endpoints
+      </a>.
+    </p>
+    <p>
+      Read more about it on{' '}
+      <a href="https://medium.com/@jmperezperez/collaborative-listening-on-spotify-using-connect-endpoints-7695603e17d1">
+        my blog post on Medium
+      </a>.
+    </p>
+    <p>
+      You can see the source code <a href="https://github.com/JMPerez/c">on GitHub</a>.
     </p>
   </Layout>
 );
