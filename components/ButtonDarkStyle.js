77ba14c710db18dd5c1a9996f560e8ea0a16fade
Jean-Francis Bastien <bhacaz@gmail.com>
2018-11-29 14:15:31 -0500

    Hover button color

--- components/ButtonDarkStyle.js
+++ components/ButtonDarkStyle.js
@@ -5,5 +5,12 @@ export default css`
     background-color: #1db954;
     border: 1px solid #1db954;
     color: #ffffff;
+    margin: 2px;
+  }
+
+  button.btn--dark:hover {
+    margin: 0;
+    background-color: #2fd566;
+    border: 3px solid #2fd566;
   }
 `;
