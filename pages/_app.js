29b6a98fe507f88bd63dd2b4b65f31c7eff77880
Jose M. Perez <JMPerez@users.noreply.github.com>
2020-11-07 09:30:30 +0100

    Add Google site verification

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,10 @@ export default function App({ Component, pageProps }) {
           name="viewport"
           content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
         />
+        <meta
+          name="google-site-verification"
+          content="XXYqCIYYKZhJRSo9Lq2J7t6om3QNtVCa6yPCyWXX9lY"
+        />
       </Head>
       <Provider store={store}>
         <Component {...pageProps} />
